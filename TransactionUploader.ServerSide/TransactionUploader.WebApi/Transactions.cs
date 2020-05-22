8b54b424d1b32c73f1789c59180e5d5eb8bea43a
dzyubaorest <dzyubaorest@gmail.com>
2020-05-22 13:38:27 +0300

    formatted transaction

--- TransactionUploader.ServerSide/TransactionUploader.WebApi/Transactions.cs
+++ TransactionUploader.ServerSide/TransactionUploader.WebApi/Transactions.cs
@@ -21,7 +21,7 @@ namespace TransactionUploader.WebApi
 				Date = new DateTime(2020, 5, 21, 13, 45, 10),
 				Status = "A"
 			},
-									new Transaction
+			new Transaction
 			{
 				Amount = 198.5m,
 				CurrencyCode = "UAH",
@@ -42,7 +42,7 @@ namespace TransactionUploader.WebApi
 				Date = new DateTime(2020, 5, 25, 13, 45, 10),
 				Status = "A"
 			},
-									new Transaction
+			new Transaction
 			{
 				Amount = 728.6m,
 				CurrencyCode = "UAH",
